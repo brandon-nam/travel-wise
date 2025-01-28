cdb4d6f2085247f61392cb9b43b974e30a4c8a0b
Heng Xing TOH <102740235+tohhengxing@users.noreply.github.com>
2025-01-28 21:35:49 +0800

    feat: Add basic test

---
+++ backend/tests/conftest.py
@@ -0,0 +1,16 @@
+import os
+import pytest
+
+from backend.server.main import create_app
+
+@pytest.fixture
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+    yield app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
