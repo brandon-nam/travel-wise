8c33616b6d253e0b5cda77d084211f019c02ffc4
Gerald Neo <gerald.ner56@gmail.com>
2025-01-28 22:07:48 +0800

    format test files

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -3,13 +3,17 @@ import pytest
 
 from backend.server.main import create_app
 
+
 @pytest.fixture
 def app():
     app = create_app()
-    app.config.update({
-        "TESTING": True,
-    })
+    app.config.update(
+        {
+            "TESTING": True,
+        }
+    )
     yield app
+
 
 @pytest.fixture
 def client(app):
--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -1,3 +1,3 @@
 def test_hello(client):
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
