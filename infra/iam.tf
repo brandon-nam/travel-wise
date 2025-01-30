15a9b067feda2fd8f82e9c40288e68b88a8f51c7
Heng Xing TOH <102740235+tohhengxing@users.noreply.github.com>
2025-01-30 19:54:01 +0800

    feat: Add member bindings to service account

--- infra/iam.tf
+++ infra/iam.tf
@@ -19,32 +19,29 @@ resource "google_iam_workload_identity_pool_provider" "cs3203" {
   }
 }
 
-# import {
-#   id = "projects/evident-trees-449214-s9/serviceAccounts/cs3203-github-actions@evident-trees-449214-s9.iam.gserviceaccount.com roles/iam.serviceAccountTokenCreator user:tohhengxing.work@gmail.com"
-#   to = google_service_account_iam_member.github_actions_workload_identity_user
-# }
-
 resource "google_service_account" "github_actions" {
   account_id = "cs3203-github-actions"
   description = "god account for now"
   display_name = "cs3203 github actions"
 }
 
-# resource "google_service_account_iam_member" "github_actions_workload_identity_user" {
-#   service_account_id = google_service_account.github_actions.name
-#   role = "roles/iam.serviceAccountTokenCreator"
-#   member = "user:tohhengxing.work@gmail.com"
-# }
+module "sa_members_github_actions_account_token_creator" {
+  source = "./modules/service_account_iam_members"
 
-# resource "google_service_account_iam_member" "github_actions_workload_id" {
-#   service_account_id = google_service_account.github_actions.name
-#   role = "roles/iam.workloadIdentityUser"
-#   member = "principalSet://iam.googleapis.com/projects/369525575533/locations/global/workloadIdentityPools/cs3203-team-1/attribute.repository/nus-cs3203/cs3203-2420-cs3203_01"
-# }
-#
-# resource "google_service_account_iam_member" "github_actions_workload_identity_user" {
-#   service_account_id = google_service_account.github_actions.name
-#   role = "roles/iam.workloadIdentityUser"
-#   member = "serviceAccount:cs3203-github-actions@evident-trees-449214-s9.iam.gserviceaccount.com"
-# }
+  service_account_id = google_service_account.github_actions.name
+  role = "roles/iam.serviceAccountTokenCreator"
+  members = [
+    "user:tohhengxing.work@gmail.com",
+  ]
+}
 
+module "sa_members_github_actions_workload_identity_user" {
+  source = "./modules/service_account_iam_members"
+
+  service_account_id = google_service_account.github_actions.name
+  role = "roles/iam.workloadIdentityUser"
+  members = [
+    "serviceAccount:cs3203-github-actions@evident-trees-449214-s9.iam.gserviceaccount.com",
+    "principalSet://iam.googleapis.com/projects/369525575533/locations/global/workloadIdentityPools/cs3203-team-1/attribute.repository/nus-cs3203/cs3203-2420-cs3203_01",
+  ]
+}
