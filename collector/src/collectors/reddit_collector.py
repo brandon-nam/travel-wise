4f17aed2cfca257e8a09e12cf20ea8aed6df239b
Gerald Neo <gerald.ner56@gmail.com>
2025-02-01 13:29:22 +0800

    use list comprehension for collector

--- collector/src/collectors/reddit_collector.py
+++ collector/src/collectors/reddit_collector.py
@@ -17,8 +17,8 @@ class RedditCollector(Collector):
     def __init__(self) -> None:
         load_dotenv()
         self.reddit = praw.Reddit(
-            client_id=os.getenv('REDDIT_CLIENT_ID'),
-            client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
+            client_id=os.getenv("REDDIT_CLIENT_ID"),
+            client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
             user_agent="collector",
         )
 
@@ -33,27 +33,23 @@ class RedditCollector(Collector):
 
     def fetch_hot_posts(self, subreddit_name: str, limit: int) -> list[dict[str, Any]]:
         subreddit = self.reddit.subreddit(subreddit_name)
-        posts = []
-        for post in subreddit.hot(limit=limit):
-            posts.append(
-                {
-                    "title": post.title,
-                    "id": post.id,
-                    "url": post.url,
-                    "score": post.score,
-                    "comments": fetch_comments_from_post(post),
-                }
-            )
+        posts = [
+            {
+                "title": post.title,
+                "id": post.id,
+                "url": post.url,
+                "score": post.score,
+                "comments": fetch_comments_from_post(post),
+            }
+            for post in subreddit.hot(limit=limit)
+        ]
         return posts
 
 
 def fetch_comments_from_post(post: Submission) -> list[dict[str, Any]]:
     post.comments.replace_more(limit=0)
-    comments = []
-
-    for comment in post.comments.list():
-        comments.append(
-            {"id": comment.id, "body": comment.body, "score": comment.score}
-        )
-
+    comments = [
+        {"id": comment.id, "body": comment.body, "score": comment.score}
+        for comment in post.comments.list()
+    ]
     return comments
