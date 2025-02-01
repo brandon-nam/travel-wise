f97e7b93d16ca7304586f9668b618b83c05a1955
Gerald Neo <gerald.ner56@gmail.com>
2025-02-01 16:28:03 +0800

    add nested replies

--- collector/src/main.py
+++ collector/src/main.py
@@ -14,13 +14,15 @@ COLLECTOR_CLASSES = [RedditCollector]
 def main():
     storage = LocalStorage()
     for collector_class in COLLECTOR_CLASSES:
-        logger.info(f"Begin: Collecting from  {collector_class.__name__}")
+        logger.info(f"Begin: Collecting from {collector_class.__name__}")
         collector = collector_class()
         file_paths = []
         for file_path, data in collector.collect():
             file_paths.append(file_path)
             storage.save(file_path=file_path, data=data)
-        logger.info(f"Successfully saved {len(file_paths)} files using {storage.__class__.__name__}: {file_paths}")
+        logger.info(
+            f"Successfully saved {len(file_paths)} files using {storage.__class__.__name__}: {file_paths}"
+        )
 
 
 if __name__ == "__main__":
