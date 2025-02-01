2b778bb7c1edb0c0c5684255375f045997bf5ad4
Gerald Neo <gerald.ner56@gmail.com>
2025-02-01 14:27:15 +0800

    fix typehint

--- collector/src/collectors/collector.py
+++ collector/src/collectors/collector.py
@@ -1,10 +1,10 @@
 from abc import ABC, abstractmethod
-from typing import Generator
+from typing import Generator, Any
 
 
 class Collector(ABC):
     @abstractmethod
-    def collect(self) -> Generator[tuple[str, list], None, None]:
+    def collect(self) -> Generator[tuple[str, Any], None, None]:
         """
         Generator method that yields tuples of (file_path, data), to be saved into storage.
         The data provided must be JSON-serializable python objects.
