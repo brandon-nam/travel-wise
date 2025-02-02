7b3281df95c95b7cb5884a1161251e0beee0f849
Heng Xing TOH <102740235+tohhengxing@users.noreply.github.com>
2025-02-02 17:33:56 +0800

    feat: Add necessary permissions for SA to push to repository

--- infra/artifact-repository.tf
+++ infra/artifact-repository.tf
@@ -3,3 +3,11 @@ resource "google_artifact_registry_repository" "this" {
   repository_id = "cs3203-repo"
   format        = "DOCKER"
 }
+
+resource "google_artifact_registry_repository_iam_member" "member" {
+  project = google_artifact_registry_repository.this.project
+  location = google_artifact_registry_repository.this.location
+  repository = google_artifact_registry_repository.this.name
+  role = "roles/artifactregistry.writer"
+  member = "serviceAccount:${google_service_account.github_actions.email}"
+}
