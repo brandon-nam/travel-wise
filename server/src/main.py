f66f7d297f4706edd3f47f7c301e464a9c9d32ab
Gerald Neo <gerald.ner56@gmail.com>
2025-02-03 17:34:09 +0800

    add cors to app

--- server/src/main.py
+++ server/src/main.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 from src.routes import create_routes
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    CORS(app)
     create_routes(app)
     return app
 
