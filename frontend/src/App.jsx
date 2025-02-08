9a019b0e675c8f0a234d23c0529a23ccca0104b0
brandon-nam <brandonnam2020@gmail.com>
2025-02-08 16:04:12 +0800

    Replace material UI with tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import LandingPage from "./LandingPage.jsx";
 function App() {
-    return <LandingPage />;
+    return (
+    <div className="flex items-center justify-center min-h-screen min-w-full bg-gray-100 p-4">
+        <LandingPage />
+    </div>
+    );
 }
 
 export default App;
--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -1,42 +1,45 @@
-import Button from "@mui/material/Button";
-import { TextField, Container, Paper, Box } from "@mui/material";
 import { useState } from "react";
 import axios from "axios";
 
 function LandingPage() {
     const [text, setText] = useState("");
-    const [result, setResult] = useState("")
+    const [result, setResult] = useState("");
 
     async function handleClick() {
-        const result = await axios.post("http://localhost:3203/query", {input: text});
-        setResult(result.data)
+        const result = await axios.get("http://localhost:3203/posts", { input: text });
+        setResult(result.data);
     }
 
-
     return (
-        <Container sx= {{height: "100vh", display: "flex", flexDirection: "column"}}>
-            <Box sx={{flex: 1, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-                <h1>Welcome to Travel Advisor</h1>
-                <p>Get started by asking me for travel suggestions</p>
-                {"Server Response: " + JSON.stringify(result)}
-            </Box>
-            <Paper
-                sx={{
-                    position: "fixed",
-                    bottom: 20,
-                    width: "80%",
-                    display: "flex",
-                    alignItems: "center",
-                    gap: 1,
-                    backgroundColor: "white",
-                }}
-            >
-                <TextField fullWidth placeholder="Ask me travel suggestions" id="fullWidth" sx={{ height: "55px" }} onChange={e => setText(e.target.value)} />
-                <Button variant="contained" sx={{ height: "55px"}} onClick={handleClick}>
+        <div className="flex flex-col items-center space-y-4 p-6 w-full max-w-5xl">
+            <div className="text-xl font-semibold text-center text-gray-800">
+                <h1 className="text-4xl font-bold">Welcome to Travelwise</h1>
+                <p className="text-lg">Get started by asking me for travel suggestions</p>
+            </div>
+            <div className="flex w-full flex-col space-y-10 text-left">
+                <input
+                    type="text"
+                    placeholder="Ask me travel suggestions"
+                    className="w-full h-14 px-4 border shadow border-gray-300 rounded-full"
+                    onChange={(e) => setText(e.target.value)}
+                />
+                <label>
+                    Select a country
+                    <select className="w-full h-14 px-4 border shadow border-gray-300 rounded-full">
+                        <option value="usa">Japan</option>
+                    </select>
+                </label>
+
+                <label>
+                    off-the-beaten value
+                    <input type="range" className="w-full"/>
+                </label>
+
+                <button onClick={handleClick} className="h-14 mx-30 bg-blue-500 text-white rounded-full hover:bg-blue-600 shadow">
                     Submit
-                </Button>
-            </Paper>
-        </Container>
+                </button>
+            </div>
+        </div>
     );
 }
 
