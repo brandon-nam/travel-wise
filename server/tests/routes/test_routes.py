c34f232f76da91f7e781e729b906c4c018f78521
Gerald Neo <gerald.ner56@gmail.com>
2025-02-13 14:28:31 +0800

    add test for routes.py

---
+++ server/tests/routes/test_routes.py
@@ -0,0 +1,36 @@
+import pytest
+from unittest.mock import MagicMock
+from flask import Flask
+from src.database.base_db import BaseDB
+from src.routes.routes import create_routes
+
+
+mock_get_posts_resp = b"mocked post response"
+mock_get_comments_resp = b"mocked comments response"
+
+
+@pytest.fixture
+def app():
+    app = Flask(__name__)
+    database = MagicMock(spec=BaseDB)
+    database.get_posts.return_value = mock_get_posts_resp
+    database.get_comments.return_value = mock_get_comments_resp
+    create_routes(app, database)
+    return app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+def test_get_posts(client):
+    response = client.get("/posts")
+    assert response.status_code == 200
+    assert response.data == mock_get_posts_resp
+
+
+def test_get_comments(client):
+    response = client.get("/comments")
+    assert response.status_code == 200
+    assert response.data == mock_get_comments_resp
