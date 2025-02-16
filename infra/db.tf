494685b2cd9d6843761bc4d2de9b6ee68c1a3904
Heng Xing TOH <102740235+tohhengxing@users.noreply.github.com>
2025-02-16 20:38:30 +0800

    feat: Add some db tf

--- infra/db.tf
+++ infra/db.tf
@@ -3,8 +3,14 @@ resource "google_compute_network" "this" {
   name = "cs3203-network"
 }
 
-resource "google_compute_global_address" "private_ip_address" {
+resource "google_compute_subnetwork" "this" {
+  name          = "cs3203-subnet"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = var.region
+  network       = google_compute_network.this.id
+}
 
+resource "google_compute_global_address" "private_ip_address" {
   name          = "private-ip-address"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
@@ -13,30 +19,41 @@ resource "google_compute_global_address" "private_ip_address" {
 }
 
 resource "google_service_networking_connection" "private_vpc_connection" {
-
   network                 = google_compute_network.this.id
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
 }
 
-resource "random_id" "db_name_suffix" {
-  byte_length = 4
-}
-
-resource "google_sql_database_instance" "instance" {
-
-  name             = "private-instance-${random_id.db_name_suffix.hex}"
-  region           = "us-central1"
-  database_version = "MYSQL_5_7"
+resource "google_sql_database_instance" "this" {
+  name             = "cs3203-db-instance"
+  database_version = "POSTGRES_14"
+  region           = var.region
 
   depends_on = [google_service_networking_connection.private_vpc_connection]
 
   settings {
     tier = "db-f1-micro"
     ip_configuration {
-      ipv4_enabled                                  = false
-      private_network                               = google_compute_network.this.self_link
+      ipv4_enabled = false
+      private_network = google_compute_network.this.id
       enable_private_path_for_google_cloud_services = true
     }
   }
+}
+
+
+resource "google_sql_user" "this" {
+  name     = "cs3203-user"
+  instance = google_sql_database_instance.this.name
+  password = "cs3203-password"
+}
+
+
+resource "google_vpc_access_connector" "this" {
+  name         = "db-connector"
+  region       = var.region
+  network      = google_compute_network.this.id
+  min_instances = 2
+  ip_cidr_range = "10.8.0.0/28"
+  max_instances = 3
 }
