653b354e604fbb8d72a4fc25b34550f4980118fe
Gerald Neo <gerald.ner56@gmail.com>
2025-02-24 14:04:48 +0800

    make collector use fs-access

--- collector/src/main.py
+++ collector/src/main.py
@@ -1,7 +1,9 @@
+import json
 import logging
 
+from fs_access.local_fs_access.local_fs_access import LocalFSAccess
+
 from collectors.reddit_collector import RedditCollector
-from storage.local_storage import LocalStorage
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
@@ -14,15 +16,16 @@ COLLECTORS = [
 
 
 def main():
-    storage = LocalStorage()
+    fs_access = LocalFSAccess()
     for collector in COLLECTORS:
         logger.info(f"Begin: Collecting from {collector.__class__.__name__}")
         file_paths = []
         for file_path, data in collector.collect():
             file_paths.append(file_path)
-            storage.save(file_path=file_path, data=data)
+            with fs_access.open(file_path, "w") as f:
+                json.dump(data, f, indent=4)
         logger.info(
-            f"Successfully saved {len(file_paths)} files using {storage.__class__.__name__}: {file_paths}"
+            f"Successfully saved {len(file_paths)} files using {fs_access.__class__.__name__}: {file_paths}"
         )
 
 
--- collector/src/storage/local_storage.py
+++
@@ -1,9 +0,0 @@
-import json
-
-from storage.storage import Storage
-
-
-class LocalStorage(Storage):
-    def save(self, file_path: str, data) -> None:
-        with open(file_path, "w") as f:
-            json.dump(data, f, indent=4)
