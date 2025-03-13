5036541e1903557049e6b0d4c88c169bccfb5917
brandon-nam <brandonnam2020@gmail.com>
2025-03-13 22:56:19 +0800

    Add post relationship to comment and bump version to 0.8

--- travelwise-common/src/database/sqlalchemy/models.py
+++ travelwise-common/src/database/sqlalchemy/models.py
@@ -45,6 +45,8 @@ class Comment(Base):
     locations = relationship(
         "Location", backref="comment", cascade="all, delete-orphan"
     )
+    
+    post = relationship("Post", backref="comments")
 
 
 class Location(Base):
