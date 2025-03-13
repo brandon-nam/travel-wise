aa4e17bb9bddc4a77e831b4c6eb7ede8714ef8d6
brandon-nam <brandonnam2020@gmail.com>
2025-03-13 17:42:50 +0800

    Show karma on the TipCard

--- frontend/src/components/TipCard.jsx
+++ frontend/src/components/TipCard.jsx
@@ -7,7 +7,10 @@ function TipCard({ tip }) {
 
     return (
         <div className={`flex flex-col shadow items-center bg-white m-3 rounded`}>
-            <div className="h-10px w-full px-5 pt-3 text-gray-400">{tip.characteristic}</div>
+            <div className="flex flex-row justify-between  min-h-10px h-10px w-full px-3 py-2 text-gray-400">
+                <div>{"# " + tip.characteristic}</div>
+                <div>{"Karma: " + tip.score}</div>
+            </div>
             <div className="pt-5 w-full text-center px-10 pb-10">{tip.summary}</div>
             <div onClick={() => handleClickTipDetails(tip)} className="w-full text-right px-3 py-2 text-blue-300 cursor-pointer hover:text-blue-500 transition">
                 See details
