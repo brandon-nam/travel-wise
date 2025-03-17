285ff7096c99c8fef32bf2a90b26e5795618993f
Gerald Neo <gerald.ner56@gmail.com>
2025-03-17 21:07:42 +0800

    add more dummy files

--- loader/src/writers/sqlalchemy_writer/sqlalchemy_writer.py
+++ loader/src/writers/sqlalchemy_writer/sqlalchemy_writer.py
@@ -1,13 +1,19 @@
+import logging
 from contextlib import contextmanager
 
 from constants.reddit import ClassificationType
 from database.sqlalchemy.models import Base, Post, Comment, Location
 from database.sqlalchemy.repository import Repository
 from sqlalchemy import create_engine
-from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from sqlalchemy.orm import Session, sessionmaker
 
 from src.writers.base_writer import BaseWriter
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 
 class SQLAlchemyWriter(BaseWriter):
@@ -36,40 +42,55 @@ class SQLAlchemyWriter(BaseWriter):
         with self.create_session() as session:
             repo = Repository(session)
             for post in json_data:
-                repo.add(
-                    Post,
-                    id=post["id"],
-                    title=post["title"],
-                    url=post["url"],
-                    score=post["score"],
-                    num_comments=post["num_comments"],
-                    country=post["country"],
-                )
+                try:
+                    repo.add(
+                        Post,
+                        id=post["id"],
+                        title=post["title"],
+                        url=post["url"],
+                        score=post["score"],
+                        num_comments=post["num_comments"],
+                        country=post["country"],
+                    )
+                except IntegrityError as e:
+                    logger.info(
+                        f"IntegrityError {e}, ignoring post with id={post['id']}"
+                    )
 
     def write_comments(self, json_data: list[dict]) -> None:
         with self.create_session() as session:
             repo = Repository(session)
             for comment in json_data:
-                repo.add(
-                    Comment,
-                    id=comment["id"],
-                    post_id=comment["post_id"],
-                    body=comment["body"],
-                    score=comment["score"],
-                    classification=ClassificationType[comment["classification"]],
-                    start_date=comment["start_date"],
-                    end_date=comment["end_date"],
-                    characteristic=comment["characteristic"],
-                    summary=comment["summary"],
-                )
+                try:
+                    repo.add(
+                        Comment,
+                        id=comment["id"],
+                        post_id=comment["post_id"],
+                        body=comment["body"],
+                        score=comment["score"],
+                        classification=ClassificationType[comment["classification"]],
+                        start_date=comment["start_date"],
+                        end_date=comment["end_date"],
+                        characteristic=comment["characteristic"],
+                        summary=comment["summary"],
+                    )
+                except IntegrityError as e:
+                    logger.info(
+                        f"IntegrityError {e}, ignoring comment with id={comment['id']}"
+                    )
 
             for comment in json_data:
                 for loc in comment["locations"]:
-                    repo.add(
-                        Location,
-                        comment_id=comment["id"],
-                        lat=loc["lat"],
-                        lng=loc["lng"],
-                        location_name=loc["location_name"],
-                        characteristic=loc["characteristic"],
-                    )
+                    try:
+                        repo.add(
+                            Location,
+                            comment_id=comment["id"],
+                            lat=loc["lat"],
+                            lng=loc["lng"],
+                            location_name=loc["location_name"],
+                            characteristic=loc["characteristic"],
+                        )
+                    except IntegrityError as e:
+                        logger.info(
+                            f"IntegrityError {e}, ignoring location ({loc['lat']}, {loc['lng']})with id={comment['id']}"
+                        )
